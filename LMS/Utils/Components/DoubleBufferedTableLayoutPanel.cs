6cf4e10723229337021633eb42ccbc005548b1fd
sandun prasanna <sandunpmapa@gmail.com>
2022-07-15 18:00:54 +0530

    :sparkles: Added TableLayoutPanel to DoubleBuffer

---
+++ LMS/Utils/Components/DoubleBufferedTableLayoutPanel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LMS.Utils.Components {
+    public class DoubleBufferedTableLayoutPanel : TableLayoutPanel {
+        public DoubleBufferedTableLayoutPanel()
+            : base() {
+            this.SetStyle(ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true);
+        }
+    }
+}
