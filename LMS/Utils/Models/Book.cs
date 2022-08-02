b8daf8048241430705515e8c4ef8b85ce5324920
sandun prasanna <sandunpmapa@gmail.com>
2022-08-02 13:44:32 +0530

    :rocket: Added Models

---
+++ LMS/Utils/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Utils.Models {
+    internal class Book {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ LMS/Utils/Models/Member.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Utils.Models {
+    internal class Member {
+        public string MID { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Address { get; set; }
+        public string Category { get; set; }
+    }
+}
