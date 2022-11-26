ad5d072c1e24a0dcab31c0746c236c9c24c23494
sandun prasanna <sandunpmapa@gmail.com>
2022-11-26 14:16:20 +0530

    :ok_hand: Complete BooksAcceptForm

---
+++ LMS/BooksAcceptForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace LMS {
+    partial class BooksAcceptForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.FineFeeTB = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RemainTB = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.RecievedTB = new Guna.UI2.WinForms.Guna2TextBox();
+            this.SuspendLayout();
+            // 
+            // Label6
+            // 
+            this.Label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Label6.AutoSize = true;
+            this.Label6.Font = new System.Drawing.Font("Proxima Nova Rg", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label6.ForeColor = System.Drawing.Color.White;
+            this.Label6.Location = new System.Drawing.Point(14, 38);
+            this.Label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(101, 25);
+            this.Label6.TabIndex = 3;
+            this.Label6.Text = "Fine Fee";
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 20;
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // FineFeeTB
+            // 
+            this.FineFeeTB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.FineFeeTB.Animated = true;
+            this.FineFeeTB.BorderRadius = 10;
+            this.FineFeeTB.BorderThickness = 3;
+            this.FineFeeTB.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.FineFeeTB.DefaultText = "";
+            this.FineFeeTB.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.FineFeeTB.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.FineFeeTB.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.FineFeeTB.DisabledState.Parent = this.FineFeeTB;
+            this.FineFeeTB.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.FineFeeTB.Enabled = false;
+            this.FineFeeTB.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.FineFeeTB.FocusedState.Parent = this.FineFeeTB;
+            this.FineFeeTB.Font = new System.Drawing.Font("Fira Code Retina", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FineFeeTB.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.FineFeeTB.HoverState.Parent = this.FineFeeTB;
+            this.FineFeeTB.Location = new System.Drawing.Point(136, 19);
+            this.FineFeeTB.Margin = new System.Windows.Forms.Padding(17, 18, 17, 18);
+            this.FineFeeTB.Name = "FineFeeTB";
+            this.FineFeeTB.PasswordChar = '\0';
+            this.FineFeeTB.PlaceholderText = "";
+            this.FineFeeTB.SelectedText = "";
+            this.FineFeeTB.ShadowDecoration.Parent = this.FineFeeTB;
+            this.FineFeeTB.Size = new System.Drawing.Size(208, 58);
+            this.FineFeeTB.TabIndex = 2;
+            this.FineFeeTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Proxima Nova Rg", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(14, 218);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Remain";
+            // 
+            // RemainTB
+            // 
+            this.RemainTB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.RemainTB.Animated = true;
+            this.RemainTB.BorderRadius = 10;
+            this.RemainTB.BorderThickness = 3;
+            this.RemainTB.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.RemainTB.DefaultText = "--";
+            this.RemainTB.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.RemainTB.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.RemainTB.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.RemainTB.DisabledState.Parent = this.RemainTB;
+            this.RemainTB.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.RemainTB.Enabled = false;
+            this.RemainTB.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.RemainTB.FocusedState.Parent = this.RemainTB;
+            this.RemainTB.Font = new System.Drawing.Font("Fira Code Retina", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RemainTB.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.RemainTB.HoverState.Parent = this.RemainTB;
+            this.RemainTB.Location = new System.Drawing.Point(136, 201);
+            this.RemainTB.Margin = new System.Windows.Forms.Padding(17, 18, 17, 18);
+            this.RemainTB.Name = "RemainTB";
+            this.RemainTB.PasswordChar = '\0';
+            this.RemainTB.PlaceholderText = "";
+            this.RemainTB.SelectedText = "";
+            this.RemainTB.SelectionStart = 2;
+            this.RemainTB.ShadowDecoration.Parent = this.RemainTB;
+            this.RemainTB.Size = new System.Drawing.Size(208, 58);
+            this.RemainTB.TabIndex = 1;
+            this.RemainTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.TargetControl = this;
+            // 
+            // RecievedTB
+            // 
+            this.RecievedTB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.RecievedTB.Animated = true;
+            this.RecievedTB.BorderRadius = 10;
+            this.RecievedTB.BorderThickness = 3;
+            this.RecievedTB.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.RecievedTB.DefaultText = "";
+            this.RecievedTB.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.RecievedTB.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.RecievedTB.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.RecievedTB.DisabledState.Parent = this.RecievedTB;
+            this.RecievedTB.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.RecievedTB.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.RecievedTB.FocusedState.Parent = this.RecievedTB;
+            this.RecievedTB.Font = new System.Drawing.Font("Fira Code Retina", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RecievedTB.ForeColor = System.Drawing.Color.Black;
+            this.RecievedTB.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.RecievedTB.HoverState.Parent = this.RecievedTB;
+            this.RecievedTB.Location = new System.Drawing.Point(15, 98);
+            this.RecievedTB.Margin = new System.Windows.Forms.Padding(20, 21, 20, 21);
+            this.RecievedTB.Name = "RecievedTB";
+            this.RecievedTB.PasswordChar = '\0';
+            this.RecievedTB.PlaceholderText = "";
+            this.RecievedTB.SelectedText = "";
+            this.RecievedTB.ShadowDecoration.Parent = this.RecievedTB;
+            this.RecievedTB.Size = new System.Drawing.Size(329, 82);
+            this.RecievedTB.TabIndex = 0;
+            this.RecievedTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.RecievedTB.KeyDown += new System.Windows.Forms.KeyEventHandler(this.RecievedTB_KeyDown);
+            this.RecievedTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.RecievedTB_KeyPress);
+            this.RecievedTB.KeyUp += new System.Windows.Forms.KeyEventHandler(this.RecievedTB_KeyUp);
+            // 
+            // BooksAcceptForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(136)))), ((int)(((byte)(221)))));
+            this.ClientSize = new System.Drawing.Size(358, 280);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.RemainTB);
+            this.Controls.Add(this.FineFeeTB);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.RecievedTB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "BooksAcceptForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BookReturnForm";
+            this.Load += new System.EventHandler(this.BooksAcceptForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BooksAcceptForm_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        internal System.Windows.Forms.Label Label6;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2TextBox FineFeeTB;
+        internal System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2TextBox RemainTB;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private Guna.UI2.WinForms.Guna2TextBox RecievedTB;
+    }
+}
---
+++ LMS/BooksAcceptForm.cs
@@ -0,0 +1,160 @@
+﻿using LMS.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LMS {
+    public partial class BooksAcceptForm : Form {
+
+        SecondForm sf;
+        private string[] values; // 0 - RefNo, 1 - ISBN, 2 - MemberID, 3 - Name, 4 - NOB
+        readonly Functions fn = new Functions();
+
+        public BooksAcceptForm(SecondForm sf, string[] values) {
+            this.sf = sf;
+            this.values = values;
+
+            InitializeComponent();
+        }
+
+        private void RecievedTB_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+                if (RemainTB.Text != "--") {
+
+                    SqlConnection conn = DBUtils.GetDBConnection();
+                    conn.Open();
+                    try {
+
+
+                        string query = "UPDATE books SET quantity = @qty WHERE isbn = @isbn;";
+
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.Add("@qty", SqlDbType.Int).Value = (fn.GetNumberOf(name: "Specified Book", value: values[1]) + 1);
+                        cmd.Parameters.Add("@isbn", SqlDbType.VarChar, 13).Value = values[1];
+
+                        string query2 = "UPDATE borrow_books SET return_date = @returnDate, status = 'Returned', fines_fee = @finesFee WHERE refno = @refNo AND mid = @mid AND isbn = @isbn;";
+                        SqlCommand cmd2 = new SqlCommand(query2, conn);
+                        cmd2.Parameters.Add("@returnDate", SqlDbType.Date).Value = DateTime.Now;
+                        cmd2.Parameters.Add("@finesFee", SqlDbType.Decimal).Value = Convert.ToDouble(FineFeeTB.Text);
+                        cmd2.Parameters.Add("@refNo", SqlDbType.VarChar, 12).Value = values[0];
+                        cmd2.Parameters.Add("@mid", SqlDbType.VarChar, 6).Value = values[2];
+                        cmd2.Parameters.Add("@isbn", SqlDbType.VarChar, 13).Value = values[1];
+
+
+
+                        if (cmd.ExecuteNonQuery() > 0 && cmd2.ExecuteNonQuery() > 0) {
+
+                            GridControlSettings dgv = new GridControlSettings();
+
+
+                            sf.SecondDgv.Columns.Clear();
+                            Console.WriteLine("NOB {0}", sf.NOB);
+                            if (sf.NOB > 1) {
+                                if (sf.SecondDgv.ColumnCount == 0) {
+
+                                    Color[] backColors = { Color.FromArgb(94, 148, 255) };
+                                    Color[] selectColors = { Color.FromArgb(120, 160, 255) };
+                                    string[] names = { "Release" };
+
+                                    dgv.GridButtons(dgv: sf.SecondDgv, names: names, backColors: backColors, selectionColors: selectColors);
+
+                                }
+                                dgv.ShowGrid(dgv: sf.SecondDgv, name: "Pending Books", searchQuery: sf.SearchTb.Text, searchQuery2: values[2]);
+                                dgv.GridWidth(dgv: sf.SecondDgv, widths: new int[] { 0, 110, 150, 200, 150, 150 });
+
+                                sf.Title2Lbl.Text = values[3] + "'s Book(s) : " + (sf.NOB -= 1);
+
+                            } else {
+                                sf.SecondDgv.Columns.Clear();
+                                if (sf.SecondDgv.ColumnCount == 0) {
+
+                                    Color[] backColors = { Color.FromArgb(77, 200, 86) };
+                                    Color[] selectColors = { Color.FromArgb(98, 222, 107) };
+                                    string[] names = { "Select Member" };
+
+                                    dgv.GridButtons(dgv: sf.SecondDgv, names: names, backColors: backColors, selectionColors: selectColors);
+
+                                }
+                                dgv.ShowGrid(dgv: sf.SecondDgv, name: "Pending Members", searchQuery: sf.SearchTb.Text);
+                                dgv.GridWidth(dgv: sf.SecondDgv, widths: new int[] { 0, 150, 200, 200 });
+
+                                sf.TitleLbl.Text = "Pending List";
+                                sf.Title2Lbl.Text = "Total Pending Books : " + fn.GetNumberOf(name: "Pending Books").ToString();
+                                sf.ActionBtn.Visible = false;
+                            }
+
+
+
+                            MessageBox.Show("Book Released!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            this.Close();
+
+                        }
+
+
+                    } catch (Exception ex) {
+                        MessageBox.Show("Book updation failed!\nTry Again", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        Console.WriteLine("Action Book Error: " + ex.ToString());
+                    } finally {
+                        conn.Close();
+                        conn.Dispose();
+                        Console.ReadLine();
+                    }
+
+                }
+            }
+        }
+
+        private void RecievedTB_KeyPress(object sender, KeyPressEventArgs e) {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && !(e.KeyChar == '.')) {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == '.' && RecievedTB.Text.IndexOf('.') > -1) {
+                e.Handled = true;
+            }
+        }
+
+        protected override CreateParams CreateParams {
+            get {
+                const int CS_DROPSHADOW = 0x20000;
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= CS_DROPSHADOW;
+                return cp;
+            }
+        }
+
+        private void BooksAcceptForm_Load(object sender, EventArgs e) {
+            FineFeeTB.Text = fn.GetFine(refNo: values[0], isbn: values[1]).ToString("00.00");
+        }
+
+        private void BooksAcceptForm_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Escape) {
+                this.Close();
+            }
+        }
+
+        private void RecievedTB_KeyUp(object sender, KeyEventArgs e) {
+            double fineFee, recieved;
+            try {
+                fineFee = Convert.ToDouble(FineFeeTB.Text);
+                recieved = (RecievedTB.Text != string.Empty) ? Convert.ToDouble(RecievedTB.Text) : 0;
+                if (fineFee <= recieved) {
+                    RemainTB.Text = (recieved - fineFee).ToString("0.00");
+                } else {
+                    RemainTB.Text = "--";
+                }
+            } catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
+            } finally {
+                Console.ReadLine();
+            }
+        }
+    }
+}
